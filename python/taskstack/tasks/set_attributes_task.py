02a5087d9b7e7de22e46de36106aed474545d4e8
kishimoto@forces.co.jp <kishimoto@forces.co.jp>
2021-10-28 15:18:37 +0900

    SetAttributesTaskサンプルタスクを追加

---
+++ python/taskstack/tasks/set_attributes_task.py
@@ -0,0 +1,23 @@
+from collections import OrderedDict
+import maya.cmds as cmds
+import pymel.core as pm
+import taskstack.core.task as task
+
+class SetAttributeTask(task.Task):
+    
+    def get_default_parameters(self):
+        return OrderedDict((
+            ('nodeType', 'transform'),
+            ('attrName', 'tx'),
+            ('value', 10),
+        ))
+        
+    def execute(self):
+        parameters = self.get_parameters()
+        nodeType = parameters.get('nodeType')
+        attrName = parameters.get('attrName')
+        value = parameters.get('value')
+        nodes = pm.ls(type=nodeType)
+        
+        for node in nodes:
+            node.attr(attrName).set(value)
