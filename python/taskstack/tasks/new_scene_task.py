de5e114e4ac4a6695427d4a9ed593a224b6bb72d
kishimoto@forces.co.jp <kishimoto@forces.co.jp>
2021-10-28 12:21:18 +0900

    NewSceneTaskのabstractmethodになったget_default_parametersメソッド対応

--- python/taskstack/tasks/new_scene_task.py
+++ python/taskstack/tasks/new_scene_task.py
@@ -3,6 +3,9 @@ import maya.cmds as cmds
 import taskstack.core.task as task
 
 class NewSceneTask(task.Task):
+
+    def get_default_parameters(self):
+        return OrderedDict()
        
     def execute(self):
         cmds.file(new=True,force=True)
