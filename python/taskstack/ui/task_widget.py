a089c3b587c554cc04f8a60f55a6575f173b50ce
kishimoto@forces.co.jp <kishimoto@forces.co.jp>
2021-10-29 21:23:17 +0900

    TaskWidgetのGroupBoxのチェックをTaskのactiveアトリビュートに紐づけ

--- python/taskstack/ui/task_widget.py
+++ python/taskstack/ui/task_widget.py
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 from pprint import pprint
 from mayaqt import maya_base_mixin, QtWidgets
 # import qtawesome as qta
@@ -15,24 +16,34 @@ class TaskWidget(maya_base_mixin, QtWidgets.QWidget):
         self.init_ui()
 
     def init_ui(self, executable=True):
+        # uiクリア
         self.clear_ui()
+
+        # Taskオブジェクトから情報取得
         task = self.__task
         doc = task.get_doc()
         param_types = self.__parameter_types
         params = task.get_parameters()
+
+        # Main layout
         self.__main_layout = QtWidgets.QVBoxLayout()
         self.setLayout(self.__main_layout)
-        self.group = QtWidgets.QGroupBox(type(task).__name__)
-        self.group.setCheckable(True)
-        self.__main_layout.addWidget(self.group)
-        group_lo = QtWidgets.QVBoxLayout()
-        self.group.setLayout(group_lo)
 
+        # Group box
+        self.group_box = QtWidgets.QGroupBox(type(task).__name__)
+        self.group_box.setCheckable(True)
+        self.group_box.setChecked(task.get_active())
+        self.group_box.toggled.connect(self.toggle_active)
+        self.__main_layout.addWidget(self.group_box)
+        group_lo = QtWidgets.QVBoxLayout()
+        self.group_box.setLayout(group_lo)
+
+        # Description
         if doc:
             doc_lbl = QtWidgets.QLabel(doc)
-            # doc_lbl.setIcon(task_icon)
             group_lo.addWidget(doc_lbl)
 
+        # Parameters
         group_form_lo = QtWidgets.QFormLayout()
         group_lo.addLayout(group_form_lo)
 
@@ -50,28 +61,32 @@ class TaskWidget(maya_base_mixin, QtWidgets.QWidget):
             group_form_lo.addRow(param_name, widget)
             self.__widgets[param_name] = widget
 
-        # if not len(param_types):
-        #     lbl = QtWidgets.QLabel('No parameters.')
-        #     group_form_lo.addRow('', lbl)
-
+        # Execute button
         if executable:
             exec_btn = QtWidgets.QPushButton('Execute')
             exec_btn.clicked.connect(self.execute)
             self.__main_layout.addWidget(exec_btn)
 
+        # Resize
         self.resize(400, 0)
 
     def clear_ui(self):
         if self.__main_layout:
             QtWidgets.QWidget().setLayout(self.__main_layout)
 
+    def toggle_active(self):
+        active = self.group_box.isChecked()
+        self.__task.set_active(active)
+
     def execute(self):
-        if not self.group.isChecked():
+        task = self.__task
+        active = task.get_active()
+
+        if not active:
             return 
 
         params = {}
         param_types = self.__parameter_types
-        task = self.__task
 
         for param_name, widget in self.__widgets.items():
             param_type = param_types.get(param_name)
