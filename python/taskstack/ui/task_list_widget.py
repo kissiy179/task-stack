b2961aad0101bfdf74eec8e3138a276e78ead66e
kissiy179 <kissiy179@gmail.com>
2021-10-30 15:53:40 +0900

    TaskListWidgetにadd_taskメソッドを仮実装

--- python/taskstack/ui/task_list_widget.py
+++ python/taskstack/ui/task_list_widget.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 from functools import partial
+from collections import OrderedDict
 from mayaqt import maya_base_mixin, QtCore, QtWidgets
 from . import WIDGET_TABLE
 import qtawesome as qta
@@ -7,6 +8,8 @@ from .task_widget import TaskWidget
 close_icon = qta.icon('fa5s.trash-alt', color='lightgray')
 up_icon = qta.icon('fa5s.chevron-up', color='lightgray')
 down_icon = qta.icon('fa5s.chevron-down', color='lightgray')
+exec_icon = qta.icon('fa5s.play', color='lightgreen')
+add_icon = qta.icon('fa5s.plus', color='white')
 
 class InnerTaskListWidget(QtWidgets.QWidget):
 
@@ -59,12 +62,21 @@ class InnerTaskListWidget(QtWidgets.QWidget):
             down_btn.clicked.connect(partial(self._movedown_task, i))
             vlo.addWidget(down_btn)
 
+            # # Spacer
+            # spacer = QtWidgets.QSpacerItem(5,10)
+            # vlo.addItem(spacer)
+
+            # # Execute button
+            # exec_btn = QtWidgets.QPushButton()
+            # exec_btn.setIcon(exec_icon)
+            # vlo.addWidget(exec_btn)
+
             # Stretch
             vlo.addStretch()
 
             # Task widget
             task_wdiget = TaskWidget(task)
-            task_wdiget.init_ui(executable=False)
+            task_wdiget.init_ui()#executable=False)
             hlo.addWidget(task_wdiget)
             self.__task_widgets.append(task_wdiget)
 
@@ -83,13 +95,14 @@ class InnerTaskListWidget(QtWidgets.QWidget):
         for task_widget in self.__task_widgets:
             task_widget.apply_parameters()
 
-class TaskListWidget(maya_base_mixin, QtWidgets.QWidget):
+class TaskListWidget(maya_base_mixin, QtWidgets.QMainWindow):
 
     def __init__(self, task_list=(), *args, **kwargs):
         super(TaskListWidget, self).__init__(*args, **kwargs)
         self.__task_list = task_list
-        print(task_list)
+        self.__toolBar = None
         self.__main_layout = None
+        self.__actions = self.get_actions()
         self.init_ui()
         self.resize(500, 600)
 
@@ -97,14 +110,18 @@ class TaskListWidget(maya_base_mixin, QtWidgets.QWidget):
         # Clear ui
         self.clear_ui()
 
+        # Main widget
+        main_wgt = QtWidgets.QWidget()
+        self.setCentralWidget(main_wgt)
+
         # Main layout
         self.__main_layout = QtWidgets.QVBoxLayout()
-        self.setLayout(self.__main_layout)
+        main_wgt.setLayout(self.__main_layout)
 
         # Scroll Aere
-        scroll_area = QtWidgets.QScrollArea()
-        scroll_area.setWidgetResizable(True)
-        self.__main_layout.addWidget(scroll_area)
+        self.scroll_area = QtWidgets.QScrollArea()
+        self.scroll_area.setWidgetResizable(True)
+        self.__main_layout.addWidget(self.scroll_area)
 
         # Parameters Widgets
         tasks = self.__task_list.get_tasks()
@@ -112,7 +129,7 @@ class TaskListWidget(maya_base_mixin, QtWidgets.QWidget):
         self.inner_wgt.remove_task.connect(self.remove_task)
         self.inner_wgt.moveup_task.connect(self.moveup_task)
         self.inner_wgt.movedown_task.connect(self.movedown_task)
-        scroll_area.setWidget(self.inner_wgt)
+        self.scroll_area.setWidget(self.inner_wgt)
 
         # Buttons
         buttons_lo = QtWidgets.QHBoxLayout()
@@ -120,17 +137,44 @@ class TaskListWidget(maya_base_mixin, QtWidgets.QWidget):
         buttons_lo.setContentsMargins(0,0,0,0)
 
         if executable:
-            exec_btn = QtWidgets.QPushButton('Execute')
-            exec_btn.clicked.connect(self.execute)
-            buttons_lo.addWidget(exec_btn)
+            self.init_toolbar()
+
+    def init_toolbar(self):
+        if self.__toolBar:
+            return 
+
+        self.__toolBar = self.addToolBar('File')
+
+        for name, action in self.__actions.items():
+            self.__toolBar.addAction(action)
+        
+    def get_actions(self):
+        actions = OrderedDict()
+        exec_action = QtWidgets.QAction(exec_icon, 'Execute', self)
+        exec_action.triggered.connect(self.execute)
+        actions['Exec'] = exec_action
+        add_task_action = QtWidgets.QAction(add_icon, 'Add Task', self)
+        add_task_action.triggered.connect(self.add_task)
+        actions['AddTask'] = add_task_action
+        return actions
         
     def clear_ui(self):
         if self.__main_layout:
             QtWidgets.QWidget().setLayout(self.__main_layout)
 
+    def add_task(self, task=None, name='NewSceneTask', parameters={}):
+        self.__task_list.add_task(task=task, name=name, parameters=parameters)
+        self.init_ui()
+        varticalBar = self.scroll_area.verticalScrollBar()
+        varticalBar.setSliderPosition(varticalBar.maximum())
+
     def remove_task(self, idx):
+        varticalBar = self.scroll_area.verticalScrollBar()
+        crr_position = varticalBar.sliderPosition()
         self.__task_list.remove_task(idx)
         self.init_ui()
+        varticalBar = self.scroll_area.verticalScrollBar()
+        varticalBar.setSliderPosition(crr_position)
 
     def moveup_task(self, idx):
         self.__task_list.moveup_task(idx)
