9383c237a658f3ac9fabbac034186e9a5285b101
kissiy179 <kissiy179@gmail.com>
2021-10-31 10:42:02 +0900

    デフォルトSpinBoxをマイナス値も受け付けるように変更

--- python/taskstack/ui/__init__.py
+++ python/taskstack/ui/__init__.py
@@ -2,17 +2,19 @@
 from mayaqt import QtWidgets
 
 class CustomSpinBox(QtWidgets.QSpinBox):
-    '''最大値を引き上げたSpinBox'''
+    '''最大最小値を引き上げたSpinBox'''
 
     def __init__(self, *args, **kwargs):
         super(CustomSpinBox, self).__init__(*args, **kwargs)
+        self.setMinimum(-100000)
         self.setMaximum(100000)
 
 class CustomDoubleSpinBox(QtWidgets.QDoubleSpinBox):
-    '''最大値を引き上げたDoubleSpinBox'''
+    '''最大最小値を引き上げたDoubleSpinBox'''
 
     def __init__(self, *args, **kwargs):
         super(CustomDoubleSpinBox, self).__init__(*args, **kwargs)
+        self.setMinimum(-100000)
         self.setMaximum(100000)
 
 
