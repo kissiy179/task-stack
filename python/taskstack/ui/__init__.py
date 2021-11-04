6f7960e7f14666566215516a7fbea776e87552b6
kissiy179 <kissiy179@gmail.com>
2021-11-05 03:04:21 +0900

    get_parameter_typesで選択できるウィジェットにFilePathEdit, DirectoryPathEdit, MayaSceneEditを追加

--- python/taskstack/ui/__init__.py
+++ python/taskstack/ui/__init__.py
@@ -1,5 +1,65 @@
 # -*- coding: utf-8 -*-
-from mayaqt import QtWidgets
+from mayaqt import QtWidgets, QtCore
+import qtawesome as qta
+import maya.cmds as cmds
+
+dir_icon = qta.icon('fa5s.folder', color='lightgray')
+
+class FilePathEdit(QtWidgets.QWidget):
+    '''
+    ファイルパス用ウィジェット
+    '''
+
+    textChanged = QtCore.Signal()
+    
+    def __init__(self, *args, **kwargs):
+        super(FilePathEdit, self).__init__(*args, **kwargs)
+        hlo = QtWidgets.QHBoxLayout()
+        hlo.setContentsMargins(0,0,0,0)
+        hlo.setSpacing(0)
+        self.setLayout(hlo)
+
+        self.line_edit = QtWidgets.QLineEdit()
+        self.line_edit.textChanged.connect(self.textChanged)
+        hlo.addWidget(self.line_edit)
+
+        self.dialog_btn = QtWidgets.QPushButton()
+        self.dialog_btn.setIcon(dir_icon)
+        self.dialog_btn.clicked.connect(self.open_dialog)
+        hlo.addWidget(self.dialog_btn)
+
+    def open_dialog(self):
+        file_obj = QtWidgets.QFileDialog.getOpenFileName()
+        file_path = file_obj[0]
+        self.setText(file_path)
+
+    def text(self):
+        return self.line_edit.text()
+
+    def setText(self, text):
+        self.line_edit.setText(text)
+
+class DirectoryPathEdit(FilePathEdit):
+    '''
+    ディレクトリパス用ウィジェット
+    '''
+
+    def open_dialog(self):
+        dir_path = QtWidgets.QFileDialog.getExistingDirectory(self)
+        self.setText(dir_path)
+
+class MayaSceneEdit(FilePathEdit):
+    '''
+    Mayaシーンパス用ウィジェット
+    .ma, .mb, .fbxが有効
+    '''
+
+    def open_dialog(self):
+        pj_path = cmds.workspace(query=True, directory=True)
+        file_obj = QtWidgets.QFileDialog.getOpenFileName(dir=pj_path, filter='Maya scene files (*.ma *.mb);;FBX files (*.fbx)')
+        file_path = file_obj[0]
+        file_path = file_path.replace(pj_path, '{}/'.format(pj_path)) # 相対パスに変換
+        self.setText(file_path)
 
 class CustomSpinBox(QtWidgets.QSpinBox):
     '''最大最小値を引き上げたSpinBox'''
@@ -17,12 +77,14 @@ class CustomDoubleSpinBox(QtWidgets.QDoubleSpinBox):
         self.setMinimum(-100000)
         self.setMaximum(100000)
 
-
 WIDGET_TABLE = {
     'bool': {'class': QtWidgets.QCheckBox, 'get_method': 'isChecked', 'set_method': 'setChecked', 'update_signal': 'stateChanged'},
     'int': {'class': CustomSpinBox, 'get_method': 'value', 'set_method': 'setValue', 'update_signal': 'valueChanged'},
     'float': {'class': CustomDoubleSpinBox, 'get_method': 'value', 'set_method': 'setValue', 'update_signal': 'valueChanged'},
     'str': {'class': QtWidgets.QLineEdit, 'get_method': 'text', 'set_method': 'setText', 'update_signal': 'textChanged'},
+    'file': {'class': FilePathEdit, 'get_method': 'text', 'set_method': 'setText', 'update_signal': 'textChanged'},
+    'dir': {'class': DirectoryPathEdit, 'get_method': 'text', 'set_method': 'setText', 'update_signal': 'textChanged'},
+    'scn': {'class': MayaSceneEdit, 'get_method': 'text', 'set_method': 'setText', 'update_signal': 'textChanged'},
 }
 
 import task_list_menu; reload(task_list_menu)
