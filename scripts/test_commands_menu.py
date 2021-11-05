a683ff47e65c7f14ab44d9a077724b2e05041188
kishimoto@forces.co.jp <kishimoto@forces.co.jp>
2021-11-05 14:43:46 +0900

    テスト用メニューを追加

---
+++ scripts/test_commands_menu.py
@@ -0,0 +1,27 @@
+#encoding: utf-8
+
+import maya.cmds as cmds
+import maya.cmds as cmds
+import reloadable_menu
+
+def show_message(message):
+    print(message)
+    cmds.inViewMessage(assistMessage=message,
+                        position='midCenter',
+                        fade=True,
+                        # fadeStayTime=3000,
+                        )
+
+def open_tasklist(*args):
+    import taskstack; reload(taskstack)
+    import taskstack.ui.task_list_widget as task_list_widget
+
+    wgt = task_list_widget.TaskListWidget()
+    wgt.show()
+
+@reloadable_menu.reloadable_menu('Test Commands')
+def create_menu():
+    # cmds.setParent('MayaWindow')
+    # cmds.menu(label='Test Commands', tearOff=True)
+    cmds.menuItem(label='TaskStack', command=open_tasklist)
+
