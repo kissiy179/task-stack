3b3b370059cde11f25fd393844f16630bd7002ce
kishimoto@forces.co.jp <kishimoto@forces.co.jp>
2022-02-24 13:42:16 +0900

    FilePathEditなどのウィジェットをpyside_compoenntsパッケージに移譲

--- python/taskstack/ui/__init__.py
+++ python/taskstack/ui/__init__.py
@@ -5,83 +5,9 @@ from mayaqt import QtWidgets, QtCore
 import qtawesome as qta
 import maya.cmds as cmds
 from .. import util
+from pyside_components.widgets import file_path_edit, directory_path_edit
 
-dir_icon = qta.icon('fa5s.folder', color='lightgray')
-
-def getOpenFileName(parent, dir='', filter=''):
-    '''
-    QtWidgets.QFileDialog.getOpenFileName が inspect.argspec に通らないので関数でラップ
-    '''
-    file_obj = QtWidgets.QFileDialog.getOpenFileName(parent, dir=dir, filter=filter)
-
-    if file_obj:
-        return file_obj[0]
-
-    return ''
-
-def getExistingDirectory(parent, dir=''):
-    '''
-    QtWidgets.QFileDialog.getExistingDirectory が inspect.argspec に通らないので関数でラップ
-    '''
-    dir = QtWidgets.QFileDialog.getExistingDirectory(parent, dir=dir)
-    return dir
-
-class FilePathEdit(QtWidgets.QWidget):
-    '''
-    ファイルパス用ウィジェット
-    '''
-
-    filter = 'All files (*)'
-    open_method = getOpenFileName
-    textChanged = QtCore.Signal()
-    
-    def __init__(self, *args, **kwargs):
-        super(FilePathEdit, self).__init__(*args, **kwargs)
-        hlo = QtWidgets.QHBoxLayout()
-        hlo.setContentsMargins(0,0,0,0)
-        hlo.setSpacing(0)
-        self.setLayout(hlo)
-
-        # LineEdit
-        self.line_edit = QtWidgets.QLineEdit()
-        self.line_edit.textChanged.connect(self.textChanged)
-        hlo.addWidget(self.line_edit)
-
-        # Dialog Button
-        self.dialog_btn = QtWidgets.QPushButton()
-        self.dialog_btn.setIcon(dir_icon)
-        self.dialog_btn.clicked.connect(self.open_dialog)
-        hlo.addWidget(self.dialog_btn)
-
-    def open_dialog(self):
-        crr_path = self.text()
-        crr_dir = os.path.dirname(crr_path) if os.path.isfile(crr_path) else crr_path
-        kwargs = {
-                'dir': crr_dir,
-                'filter': self.filter,
-                }
-        argspec = inspect.getargspec(self.open_method)
-        kwargs = {key: value for key, value in kwargs.items() if key in argspec.args}
-        result = self.open_method(**kwargs)
-
-        if result:
-            self.setText(result)
-
-        return result
-
-    def text(self):
-        return self.line_edit.text()
-
-    def setText(self, text):
-        self.line_edit.setText(text)
-
-class DirectoryPathEdit(FilePathEdit):
-    '''
-    ディレクトリパス用ウィジェット
-    '''
-    open_method = getExistingDirectory
-
-class FilePathInMayaProjectEdit(FilePathEdit):
+class FilePathInMayaProjectEdit(file_path_edit.FilePathEdit):
     '''
     Mayaプロジェクト内の場合相対パスとして記憶するファイルパス用ウィジェット
     '''
@@ -103,7 +29,7 @@ class DirectoryPathInMayaProjectEdit(FilePathInMayaProjectEdit):
     '''
     Mayaプロジェクト内の場合相対パスとして記憶するファイルパス用ディレクトリパス用ウィジェット
     '''
-    open_method = getExistingDirectory
+    open_method = directory_path_edit.getExistingDirectory
 
 class MayaSceneEdit(FilePathInMayaProjectEdit):
     '''
@@ -142,8 +68,8 @@ WIDGET_TABLE = {
     'int': {'class': CustomSpinBox, 'get_method': 'value', 'set_method': 'setValue', 'update_signal': 'valueChanged'},
     'float': {'class': CustomDoubleSpinBox, 'get_method': 'value', 'set_method': 'setValue', 'update_signal': 'valueChanged'},
     'str': {'class': QtWidgets.QLineEdit, 'get_method': 'text', 'set_method': 'setText', 'update_signal': 'textChanged'},
-    'file': {'class': FilePathEdit, 'get_method': 'text', 'set_method': 'setText', 'update_signal': 'textChanged'},
-    'dir': {'class': DirectoryPathEdit, 'get_method': 'text', 'set_method': 'setText', 'update_signal': 'textChanged'},
+    'file': {'class': file_path_edit.FilePathEdit, 'get_method': 'text', 'set_method': 'setText', 'update_signal': 'textChanged'},
+    'dir': {'class': directory_path_edit.DirectoryPathEdit, 'get_method': 'text', 'set_method': 'setText', 'update_signal': 'textChanged'},
     'file_in_pj': {'class': FilePathInMayaProjectEdit, 'get_method': 'text', 'set_method': 'setText', 'update_signal': 'textChanged'},
     'dir_in_pj': {'class': DirectoryPathInMayaProjectEdit, 'get_method': 'text', 'set_method': 'setText', 'update_signal': 'textChanged'},
     'scn': {'class': MayaSceneEdit, 'get_method': 'text', 'set_method': 'setText', 'update_signal': 'textChanged'},
